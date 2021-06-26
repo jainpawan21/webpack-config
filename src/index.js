6fc470fd675fdff355607d51900e863f9428ac80
Pawan Jain <jainpawan211199@gmail.com>
2021-06-26 16:01:45 +0530

    add babel to project

--- src/index.js
+++ src/index.js
@@ -4,4 +4,10 @@ const elvenShieldRecipe = {
   refinedMoonstone: 4,
 };
 
+const elvenGauntletsRecipe = {
+  ...elvenShieldRecipe,
+  leather: 1,
+  refinedMoonstone: 4,
+}
+
 console.log(elvenShieldRecipe);
